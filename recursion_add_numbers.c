4a27151d12aaed7d45f1366c75a7592a9c064863
TomasNagy <tomi.nagy@outlook.com>
2023-05-30 19:57:53 +0200

    feat: implement calculate sum function

--- recursion_add_numbers.c
+++ recursion_add_numbers.c
@@ -1,7 +1,23 @@
 #include <cs50.h>
 #include <stdio.h>
 
+int calculate_sum(int number);
+
 int main(void)
 {
     int number = get_int("Number: ");
+
+    int sum = calculate_sum(number);
+
+    printf ("Sum of numbers up to %i is: %i\n", number, sum);
+}
+
+int calculate_sum(int number)
+{
+    if (number <= 1)
+    {
+        return number;
+    }
+
+    return number + calculate_sum(number - 1);
 }
